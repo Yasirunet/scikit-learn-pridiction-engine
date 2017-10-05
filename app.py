eb935df8ad00cdd073b9b4678288d3a0263bc7c1
yasiru <yasiru@example.com>
2017-10-05 14:56:37 +0530

    add sklearn codes..

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask 
+from sklearn.externals import joblib
+clf = joblib.load('modle.pkl')
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def test():
-	return "flask app is working !"
+	test_list = [5,2,3,0]
+	get_pre = clf.predict([test_list])
+	pre = get_pre[0]
+	return pre
 
 if __name__ == '__main__':
 	app.run()
