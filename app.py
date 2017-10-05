f94410e1dd83bf743c7f19de66a2af60f4f7a730
yasiru <yasiru@example.com>
2017-10-05 20:27:50 +0530

    add env

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask 
 from sklearn.externals import joblib
-
+from sklearn 
 
 
 app = Flask(__name__)
 
-clf = joblib.load('modle.pkl')
+#clf = joblib.load('modle.pkl')
 
 @app.route('/')
 def test():
+
 	test_list = [5,3,2,0]
 	get_pre = clf.predict([test_list])
 	pre = get_pre[0]
