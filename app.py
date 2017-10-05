a93f6b6f727adee6aaff7c4f433dc53e9cb2f770
yasiru <yasiru@example.com>
2017-10-05 16:39:11 +0530

    app.py mod..

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask 
-#from sklearn.externals import joblib
+from sklearn.externals import joblib
 
-#clf = joblib.load('modle.pkl')
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def test():
-	#test_list = [5,3,2,0]
-	#get_pre = clf.predict([test_list])
-	#pre = get_pre[0]
-	return "pre"
+	test_list = [5,3,2,0]
+	get_pre = clf.predict([test_list])
+	pre = get_pre[0]
+	print(pre)
 
 
 	
 if __name__ == '__main__':
+	clf = joblib.load('modle.pkl')
 	app.run()
+	
