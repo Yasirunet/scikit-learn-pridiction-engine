91f2cf2b913ab46cc151c29276312e9bf148d1e5
yasiru <yasiru@example.com>
2017-10-05 16:32:19 +0530

    remove sklearn...

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask 
-from sklearn.externals import joblib
+#from sklearn.externals import joblib
 
-clf = joblib.load('modle.pkl')
+#clf = joblib.load('modle.pkl')
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def test():
-	test_list = [5,3,2,0]
-	get_pre = clf.predict([test_list])
-	pre = get_pre[0]
-	return pre
+	#test_list = [5,3,2,0]
+	#get_pre = clf.predict([test_list])
+	#pre = get_pre[0]
+	return "pre"
 
 
 	
