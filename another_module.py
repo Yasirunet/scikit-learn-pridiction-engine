29c807a934df8cab42b93f62225cb09147853b40
yasiru <yasiru@example.com>
2017-10-06 11:24:22 +0530

    commited..

--- another_module.py
+++ another_module.py
@@ -4,12 +4,12 @@ clf = joblib.load('model.pkl')
 
 
 def get_value(*args):
-        test_list = [5,3,2,0]
+	test_list = [5,3,2,0]
 	
-        get_pre = clf.predict([test_list])
-        pre = get_pre[0]
+	get_pre = clf.predict([test_list])
+	pre = get_pre[0]
 	
-        return pre + ":".join(map(str, args))
+        return "Hello World " + ":".join(map(str, args))
 
 def main(argv):
     print(get_value(*argv[1:]))
