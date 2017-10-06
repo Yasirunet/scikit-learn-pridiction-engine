719dc902e8c63c00e8f26532d484d3374a0e9720
yasiru <yasiru@example.com>
2017-10-06 17:14:22 +0530

    delete some scripts!

--- predict.py
+++
@@ -1,9 +0,0 @@
-#from sklearn.externals import joblib
-#clf = joblib.load('model.pkl')
-
-#def get_pred():
-#	test_list = [5,3,2,0]
-#	
-#	get_pre = clf.predict([test_list])
-#	pre = get_pre[0]
-#	return pre
