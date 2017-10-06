34033cd6ccebd5e117246d46b21b58fec699139c
yasiru <yasiru@example.com>
2017-10-06 21:15:34 +0530

    comm

--- another_module.py
+++ another_module.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 from sklearn.externals import joblib
 from sklearn import datasets
-#from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
-clf = joblib.load('model2.pkl')
+#clf = joblib.load('model2.pkl')
 
 
 def main():
 
     iris = datasets.load_iris()
 
-    #x = iris.data
-    #y = iris.target
+    x = iris.data
+    y = iris.target
 	
-    #knn = KNeighborsClassifier()
-    #clf = knn.fit(x,y)
+    knn = KNeighborsClassifier()
+    clf = knn.fit(x,y)
     test_list = [10,3,2,10]
 	
     get_pre = clf.predict([test_list])
